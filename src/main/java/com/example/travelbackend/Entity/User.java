f8edbfdd5ec3ec7153f2df9acf97315509ae780a
Your Name <you@example.com>
2024-10-27 22:59:04 +0100

    first commit

---
+++ src/main/java/com/example/travelbackend/Entity/User.java
@@ -0,0 +1,4 @@
+package com.example.travelbackend;
+
+public class User {
+}
---
+++ src/test/java/com/example/travelbackend/TravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
